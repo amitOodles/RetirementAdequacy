06e829ab7bec81dcd0efd751ccba22bddf34aa9d
amitOodles <amit.k@oodlestechnologies.com>
2016-09-15 22:31:19 +0530

    controller changes

--- js/controller.js
+++ js/controller.js
@@ -160,9 +160,9 @@ app.controller("TTRController", ['$scope', '$timeout', 'AgeCalculator', 'TaxRate
 
     $scope.employerContributionLevel = 9.50;
 
-    $scope.inflation = 2.50;
+    $scope.inflation = 3.50;
 
-    $scope.superBalance = 100000;
+    $scope.superBalance = 500000;
 
     $scope.rateOfReturn = 5.58;
 
@@ -679,6 +679,124 @@ app.controller("TTRController", ['$scope', '$timeout', 'AgeCalculator', 'TaxRate
     //     }
     //     PdfMaker.createChart($scope.dob, $scope.age, $scope.fy, $scope.cses, $scope.thp, $scope.resultWithoutSS, $scope.resultWithSS, $scope.needSS, $scope.optimisedSS, toggleNeeded);
     // });
+
+    function calculateMinPension(age){
+        if(age >= 56 && age <=64){
+            return 4;
+        }
+        if(age >= 65 && age <=74){
+            return 5;
+        }
+        if(age >= 75 && age <=79){
+            return 6;
+        }
+        if(age >= 80 && age <=84){
+            return 7;
+        }
+        if(age >= 85 && age <=89){
+            return 9;
+        }
+        if(age >= 90 && age <=94){
+            return 11;
+        }
+        if(age >= 95){
+            return 14;
+        }
+    }
+
+    function cLookUp(sal){
+        if(sal <= 249999){
+            return 0.15;
+        }else{
+            return 0.3;
+        }
+    }
+
+    $scope.isCouple = true;
+
+    $scope.ownsHome = true;
+
+    var biArray = [];
+
+    var baArray = [];
+
+    var balanceIndexed = 0;
+
+    var year = 0;
+
+    var cpi;
+
+    var adjustedSalary,concessionalCo,earning,taxation,drawdown,fAndI,balance,balanceCpi,paymentFactor,ageL;
+
+    ageL = $scope.age;
+
+    $scope.minPension = true;
+
+    $scope.ddPercent = 4.00;
+
+    $scope.ddBase = 40000;
+
+
+
+    while(balanceIndexed >= 0){
+        cpi = Math.pow(1+($scope.inflation/100),year);
+        adjustedSalary = ageL < $scope.retirementAge ? $scope.annualSalary * Math.pow(1 + ($scope.wageIncrease/100),year) : 0;
+        if(year === 0){
+            concessionalCo = 0;
+        }else{
+            if(ageL < $scope.retirementAge){
+                var temp = adjustedSalary * ($scope.employerContributionLevel/100) + $scope.salarySacrifice;
+                concessionalCo = temp <= 25000 ? temp : 25000;
+            }else{
+                concessionalCo = 0;
+            }
+        }
+        balanceCpi = 1/cpi;
+        var temp1 = 0;
+        if(year === 0){
+            earnings = taxation = drawdown = fAndI =0;
+            balance = $scope.superBalance;
+
+        }else{
+            if($scope.minPension){
+                if(ageL < $scope.pensionStart){
+                    drawdown = 0;
+                }else{
+                   drawdown = baArray[year - 1] * (calculateMinPension(ageL)/100)
+                }
+            }else{
+                if(ageL < $scope.pensionStart){
+                    drawdown = 0;
+                }else{
+                    drawdown = $scope.ddBase * Math.pow(1 + ($scope.inflation/100), ageL - $scope.pensionStart);
+                }
+            }
+
+            fAndI = baArray[year-1] * ($scope.variableFee/100) + $scope.fixedFee + $scope.insurancePremium;
+
+            earnings = baArray[year -1] * (Math.pow(1+($scope.investmentReturn/100),0.5) - 1) + (baArray[year-1] * Math.pow(1+($scope.investmentReturn/100),0.5) +
+             concessionalCo - fAndI - drawdown ) * (Math.pow(1+($scope.investmentReturn/100),0.5) - 1);
+
+            if(ageL >=60 && ageL >= $scope.pensionStart){
+                taxation = cLookUp($scope.annualSalary) * concessionalCo;
+            }else{
+                taxation = cLookUp($scope.annualSalary) * concessionalCo + earnings * 0.15; 
+            }
+
+            balance = baArray[year-1] + concessionalCo +earnings - taxation -drawdown -fAndI;
+        }
+
+        balanceIndexed = balance * balanceCpi;
+
+        baArray.push(balance);
+
+        // biArray.push(balanceIndexed);
+
+        year++;
+
+        ageL++;
+
+    }
 
 
 }]);
